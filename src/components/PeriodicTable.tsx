d853f5b475b489d1f78d890475124860265942c1
Fredrik Johansen <fredrik1206@gmail.com>
2025-04-29 20:00:18 +0200

    chore(table): removes unused mapping of elements

--- src/components/PeriodicTable.tsx
+++ src/components/PeriodicTable.tsx
@@ -2,15 +2,6 @@ import { chemicalElements } from "../data/chemicalElements";
 import ChemicalElementCard from "./ChemicalElementCard";
 
 export const PeriodicTable = () => {
-  const gridPositions: Record<string, { gridRow: number; gridColumn: number }> =
-    {};
-  chemicalElements.forEach((chemicalElement) => {
-    gridPositions[chemicalElement.symbol] = {
-      gridRow: chemicalElement.period,
-      gridColumn: chemicalElement.group || 1,
-    };
-  });
-
   return (
     <div className="container mx-auto grid grid-cols-18 grid-rows-7 gap-4">
       {chemicalElements
