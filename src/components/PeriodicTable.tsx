e17c0748bf2139693271a7b878326a6f9dcc4f43
Fredrik Johansen <fredrik1206@gmail.com>
2025-04-29 20:24:12 +0200

    feat(table): adds numbers for period and group

--- src/components/PeriodicTable.tsx
+++ src/components/PeriodicTable.tsx
@@ -3,7 +3,32 @@ import ChemicalElementCard from "./ChemicalElementCard";
 
 export const PeriodicTable = () => {
   return (
-    <div className="container mx-auto grid grid-cols-18 grid-rows-7 gap-4">
+    <div className="container mx-auto grid grid-cols-19 grid-rows-8 gap-4">
+      {/* Group Numbers */}
+      {new Array(18).fill(null).map((_, index) => (
+        <div
+          className="flex justify-center items-center text-xl dark:text-white text-black"
+          style={{
+            gridRow: 1,
+            gridColumn: index + 2,
+          }}
+        >
+          {index + 1}
+        </div>
+      ))}
+      {/* Period Numbers */}
+      {new Array(7).fill(null).map((_, index) => (
+        <div
+          className="flex justify-center items-center text-xl dark:text-white text-black"
+          style={{
+            gridRow: index + 2,
+            gridColumn: 1,
+          }}
+        >
+          {index + 1}
+        </div>
+      ))}
+      {/* Chemical Elements */}
       {chemicalElements
         .filter((item) => item.group !== null)
         .map((chemicalElement) => (
@@ -11,8 +36,8 @@ export const PeriodicTable = () => {
             key={chemicalElement.symbol}
             element={chemicalElement}
             style={{
-              gridRow: chemicalElement.period,
-              gridColumn: chemicalElement.group || 1,
+              gridRow: chemicalElement.period + 1,
+              gridColumn: (chemicalElement.group || 1) + 1,
             }}
           />
         ))}
