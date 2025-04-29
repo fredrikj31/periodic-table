e6dd81f9a8ec4acc092d588ef6a15eb74dee95e3
Fredrik Johansen <fredrik1206@gmail.com>
2025-04-29 20:15:12 +0200

    feat(app): adds theme provider and toggler

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { PeriodicTable } from "./components/PeriodicTable";
+import { ThemeToggler } from "./components/ThemeToggler";
+import { ThemeProvider } from "./providers/theme";
 
 function App() {
   return (
-    <>
-      <PeriodicTable />
-    </>
+    <div className="bg-white dark:bg-zinc-800 w-full min-h-screen">
+      <ThemeProvider defaultTheme="light" storageKey="periodic-table-theme">
+        <PeriodicTable />
+        <ThemeToggler />
+      </ThemeProvider>
+    </div>
   );
 }
 
