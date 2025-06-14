e1a98c41ae3cc261da788e1100fa6b159b4f0dc6
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-14 09:17:58 +0200

    feat(pages): updates design for pages using shadcn-ui components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,16 @@ import { ThemeToggler } from "../components/ThemeToggler";
 
 export const HomePage = () => {
   return (
-    <>
+    <div className="flex flex-col gap-4">
+      {/* Header */}
+      <div className="grid grid-cols-3 container mx-auto items-center py-8">
+        <div />
+        <h1 className="text-5xl font-semibold text-center">Periodic Table</h1>
+        <div className="flex justify-end">
+          <ThemeToggler />
+        </div>
+      </div>
       <PeriodicTable />
-      <ThemeToggler />
-    </>
+    </div>
   );
 };
